d7f1a28414b6ace44dcdaa82c833f69ecdf991eb
RodrigoAGM <rod-gm@hotmail.com>
2019-07-05 22:27:52 -0500

    WA function implemented

--- functions.py
+++ functions.py
@@ -16,3 +16,18 @@ def normalize(data):
     maxVal = max(data)
     result = list(map(lambda x: (x - minVal) / (maxVal - minVal), data))
     return result
+
+def WA(data,weights):
+    """
+    Function that gets a dataset readed as a list of lists and a list of weights for
+    each column and returns the dataframe with the WA.
+    """
+    total = sum(weights)
+    newData = []
+    for row in data:
+        val = 0
+        for i in range(len(row)):
+            val = val + ((row[i] * weights[i])/total)
+        row.append(val)
+        newData.append(row)
+    return newData
